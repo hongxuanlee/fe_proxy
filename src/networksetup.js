38789b4623c0bc5f7e1206a1d5e0068e7d7ba535
Kino Li <kino.li@shopee.com>
2018-03-28 17:19:56 +0800

    feature: find network

--- src/networksetup.js
+++ src/networksetup.js
@@ -1,22 +1,40 @@
 const {
-    spawn
+    spawn,
+    execSync
 } = require('child_process');
 const plog = require('./plog');
 
+const log = console.log; // eslint-disable-line
+
+const findNetworkService = () => {
+    const deviceName = execSync('route get example.com | grep interface').toString().replace(/\s*interface:\s(.*)/, '$1');
+    const service = execSync(`networksetup -listnetworkserviceorder | grep ${deviceName}`).toString();
+    const serviceName = service.match(/Hardware\sPort:\s(.*),/);
+    if (serviceName) {
+        return serviceName[1];
+    }
+    return null;
+};
+
+const serviceName = findNetworkService() || 'Wi-fi';
+
+log(`network serviceName is ${serviceName}`);
+
 const setWebProxy = (host, port) => {
-    const command = spawn('networksetup', ['-setwebproxy', 'Wi-fi', host || '127.0.0.1', port || '6001']);
+    const command = spawn('networksetup', ['-setwebproxy', serviceName, host || '127.0.0.1', port || '6001']);
     plog(command);
 };
 
 const offWebProxy = () => {
-    const command = spawn('networksetup', ['-setwebproxystate', 'Wi-fi', 'off']);
+    const command = spawn('networksetup', ['-setwebproxystate', serviceName, 'off']);
     plog(command);
 };
 
 const getWebProxy = () => {
-    const command = spawn('networksetup', ['-getwebproxy', 'Wi-fi']);
+    const command = spawn('networksetup', ['-getwebproxy', serviceName]);
     plog(command);
 };
+
 
 module.exports = {
     setWebProxy,
